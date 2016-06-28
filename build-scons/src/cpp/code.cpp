ad1e68495310a136d7811678f75a14be7c81096c
revesansparole <revesansparole@gmail.com>
2016-06-28 13:57:09 +0200

    removed scons tmp dir

--- build-scons/src/cpp/code.cpp
+++
@@ -1,8 +0,0 @@
-#include "code.h"
-
-
-int code_func(int arg)
-{
-    return arg * 2;
-}
-
--- build-scons/src/wrapper/code_wrapper.cpp
+++
@@ -1,9 +0,0 @@
-#include <boost/python.hpp>
-#include "../cpp/code.h"
-
-
-BOOST_PYTHON_MODULE(_oacpp)
-{
-    using namespace boost::python;
-    def("code", code_func);
-}
